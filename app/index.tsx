7cd54572e4d1c250e4875609efe8608ed4ae69c8
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-09-06 15:44:48 +0600

    working on expo advance routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,49 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import {
-  Button,
-  ButtonText,
-  ButtonSpinner,
-  ButtonIcon,
-  ButtonGroup,
-} from "@/components/ui/button";
+import { StyleSheet, Text, View } from "react-native";
 
-import * as Linking from "expo-linking";
-import * as WebBrowser from "expo-web-browser";
-
-export default function HomePage() {
-  const url = Linking.useURL();
-
-  if (url) {
-    const { hostname, path, queryParams } = Linking.parse(url);
-
-    console.warn(
-      `host: ${hostname}, path: ${path} data: ${JSON.stringify(queryParams)}`
-    );
-  }
-
+const HomePage = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Button size="sm" variant="solid" action="positive">
-        <ButtonText>tailwind btn</ButtonText>
-      </Button>
-      <Button
-        size="sm"
-        variant="solid"
-        action="primary"
-        onPress={() => WebBrowser.openBrowserAsync("https://google.com")}
-      >
-        <ButtonText>visit google?</ButtonText>
-      </Button>
-
-      <Button
-        size="sm"
-        variant="solid"
-        action="negative"
-        onPress={() => Linking.openURL("https://facebook.com")}
-      >
-        <ButtonText>visit facebook?</ButtonText>
-      </Button>
+    <View>
+      <Text>Home page</Text>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({});
+
+export default HomePage;
