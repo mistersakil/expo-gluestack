2f12a0343eb658f74b8ddeb2b88216b6bec5bf1a
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-09-09 00:19:47 +0600

    workin gon drawar nagivation

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Index = () => {
+  return (
+    <View>
+      <Text>Home page</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Index;
