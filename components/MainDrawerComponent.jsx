d0d69f0d248438ea85e20cd905f0f9beffe2833b
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-09-10 23:44:51 +0600

    customized designed drawer component

--- components/MainDrawerComponent.jsx
+++ components/MainDrawerComponent.jsx
@@ -1,9 +1,47 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import Feather from "@expo/vector-icons/Feather";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, useNavigation } from "expo-router";
+import { DrawerActions } from "@react-navigation/native";
 
 const MainDrawerComponent = () => {
   const logoSource = require("@/assets/images/logoipsum-241.png");
+  const navigation = useNavigation();
+
+  const menuItems = [
+    {
+      title: "Home",
+      link: "/",
+      icon: <Feather name="home" size={20} color="green" />,
+    },
+    {
+      title: "About",
+      link: "/about",
+      icon: <Feather name="info" size={20} color="green" />,
+    },
+    {
+      title: "Login",
+      link: "/login",
+      icon: <MaterialIcons name="login" size={20} color="green" />,
+    },
+    {
+      title: "Register",
+      link: "/register",
+      icon: <FontAwesome name="sign-in" size={20} color="green" />,
+    },
+    {
+      title: "Profile",
+      link: "/profile",
+      icon: <Feather name="user" size={20} color="green" />,
+    },
+  ];
+
+  const closeDrawerNavigation = () => {
+    navigation.dispatch(DrawerActions.closeDrawer());
+  };
 
   return (
     <View style={{ flex: 1, paddingTop: StatusBar.currentHeight || 50 }}>
@@ -12,12 +50,20 @@ const MainDrawerComponent = () => {
           <Image style={styles.logo} source={logoSource} />
         </View>
         <View style={styles.navContainer}>
-          <Text>List of menu items</Text>
+          {menuItems.length > 0 &&
+            menuItems.map((item, index) => {
+              return (
+                <Link key={index} style={styles.navItem} href={item.link} push>
+                  {item.icon}
+                  <Text style={styles.navItemText}>{item.title}</Text>
+                </Link>
+              );
+            })}
         </View>
       </View>
-      <View style={styles.footer}>
-        <Text>Footer</Text>
-      </View>
+      <Pressable style={styles.footer} onPress={closeDrawerNavigation}>
+        <Text style={styles.footerText}>Close</Text>
+      </Pressable>
     </View>
   );
 };
@@ -25,10 +71,8 @@ const MainDrawerComponent = () => {
 const styles = StyleSheet.create({
   header: {
     flex: 10,
-    // backgroundColor: "green",
   },
   logoContainer: {
-    // backgroundColor: "orange",
     flex: 1,
     flexDirection: "column",
     alignItems: "center",
@@ -37,11 +81,29 @@ const styles = StyleSheet.create({
   },
   logo: {},
   navContainer: {
-    backgroundColor: "red",
     flex: 10,
+    padding: 15,
+    paddingTop: 40,
+    gap: 10,
+  },
+  navItem: {
+    flex: 1,
+    maxHeight: "40px",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  navItemText: {
+    fontSize: 24,
+    marginLeft: 10,
   },
   footer: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  footerText: {
+    fontSize: 24,
   },
 });
 
